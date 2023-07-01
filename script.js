49e8d82b6a1ac6460e0d92b7ff0b293dacd72346
hiltonjfn <88454528+hiltonjfn@users.noreply.github.com>
2023-07-01 02:07:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
- // Nome do arquivo de texto
- var fileName = 'seu_arquivo.txt';
+    // Quando o usuário selecionar um arquivo
+    document.getElementById('file-input').addEventListener('change', function(event) {
+      var file = event.target.files[0];
+      var reader = new FileReader();
 
- // Função para carregar o conteúdo do arquivo de texto
- function loadFile() {
-   var xhr = new XMLHttpRequest();
-   xhr.open('GET', fileName, true);
-   xhr.onreadystatechange = function() {
-     if (xhr.readyState === 4 && xhr.status === 200) {
-       var content = xhr.responseText;
-       document.getElementById('file-content').textContent = content;
-     }
-   };
-   xhr.send();
- }
+      reader.onload = function(e) {
+        var content = e.target.result;
+        document.getElementById('file-content').textContent = content;
+      };
 
- // Chama a função para carregar o arquivo quando a página é carregada
- window.onload = loadFile;
+      reader.readAsText(file);
+    });
